b215b8fe5ebe644316f86e890ecf7568041dcab7
QuintinChris <quintinchrisdev@gmail.com>
2021-04-30 09:40:37 -0400

    added navbarScrollEffect snippet

---
+++ navbarScrollEffect.js
@@ -0,0 +1,8 @@
+// detect whether user has scrolled the page down by 10px 
+useEffect(() => {
+    const scrollHandler = () => {
+      window.pageYOffset > 10 ? setTop(false) : setTop(true)
+    };
+    window.addEventListener('scroll', scrollHandler);
+    return () => window.removeEventListener('scroll', scrollHandler);
+  }, [top]);  
